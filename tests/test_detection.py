fc4ec68f3429802ac7d36a63cb3434ec851170e0
hanbing <beatmight@gmail.com>
2019-02-14 15:33:43 +0800

    change the test img

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -2,11 +2,13 @@
 Test cases for mtcnn.deploy.detect.py
 """
 import os
+import cv2
 import unittest
 
 import numpy as np
 import mtcnn.deploy.detect as detect
 import mtcnn.network.mtcnn_pytorch as mtcnn
+import mtcnn.utils.draw as draw
 
 here = os.path.dirname(__file__)
 
@@ -28,7 +30,22 @@ class TestDetection(unittest.TestCase):
             np.load(os.path.join(weight_folder, 'onet.npy'))[()])
 
         self.detector = detect.FaceDetector(pnet, rnet, onet)
-        self.test_img = os.path.join(here, 'asset/images/bksomels.jpg')
+        self.test_img = os.path.join(here, 'asset/images/office5.jpg')
 
     def test_detection(self):
         self.detector.detect(self.test_img)
+
+    def test_stage_one(self):
+        img = cv2.imread(self.test_img)
+        norm_img = self.detector._preprocess(self.test_img)
+        stage_one_boxes = self.detector.stage_one(norm_img, 0.9, 0.707, 12)
+        draw.draw_boxes2(img, stage_one_boxes)
+        cv2.imshow('Stage One Boxes', img)
+        cv2.waitKey(0)
+
+    def test_stage_two(self):
+        # Running this test case after 'test_stage_one' passed.
+        img = cv2.imread(self.test_img)
+        norm_img = self.detector._preprocess(self.test_img)
+        stage_one_boxes = self.detector.stage_one(norm_img, 0.6, 0.707, 12)
+        stage_two_boxes = self.detector.stage_two(norm_img, stage_one_boxes, 0.7)
