35d25a6ad4a54aad06413dfea62e8c7821d1003f
hanbing <beatmight@gmail.com>
2019-02-27 13:17:24 +0800

    add get_net_caffe method

--- mtcnn/__init__.py
+++ mtcnn/__init__.py
@@ -1,3 +1,3 @@
-from mtcnn.deploy import get_net
+from mtcnn.deploy import get_net, get_net_caffe
 from mtcnn.deploy.detect import FaceDetector
 from mtcnn.deploy.batch_detect import BatchImageDetector
