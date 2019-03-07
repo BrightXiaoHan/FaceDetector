07920f82d339514f3d436371873288c9eb830169
韩冰 <hanbing@hanbings-MacBook.local>
2019-03-07 17:01:37 +0800

    merge mtcnn_jit.py to mtcnn_pytorch

--- tests/test_net_jit.py
+++ tests/test_net_jit.py
@@ -3,14 +3,14 @@ import unittest
 import torch
 
 import numpy as np
-import mtcnn.network.mtcnn_jit as mtcnn
+import mtcnn.network.mtcnn_pytorch as mtcnn
 
 here = os.path.dirname(__file__)
 
 class TestMtcnnPytorch(unittest.TestCase):
 
     def test_pnet(self):
-        pnet = mtcnn.PNet()
+        pnet = mtcnn.PNet().to_script()
         data = torch.randn(100, 3, 12, 12)
 
         det, box, _ = pnet(data)
@@ -18,7 +18,7 @@ class TestMtcnnPytorch(unittest.TestCase):
         self.assertEqual(list(box.shape), [100, 4, 1, 1])
 
     def test_rnet(self):
-        rnet = mtcnn.RNet()
+        rnet = mtcnn.RNet().to_script()
         data = torch.randn(100, 3, 24, 24)
 
         det, box, _ = rnet(data)
@@ -26,22 +26,10 @@ class TestMtcnnPytorch(unittest.TestCase):
         self.assertEqual(list(box.shape), [100, 4])
 
     def test_onet(self):
-        onet = mtcnn.ONet()
+        onet = mtcnn.ONet().to_script()
         data = torch.randn(100, 3, 48, 48)
 
         det, box, landmarks = onet(data)
         self.assertEqual(list(det.shape), [100, 2])
         self.assertEqual(list(box.shape), [100, 4])
         self.assertEqual(list(landmarks.shape), [100, 10])
-
-    def test_load_caffe_model(self):
-        pnet = mtcnn.PNet()
-        rnet = mtcnn.RNet()
-        onet = mtcnn.ONet()
-        weight_folder = os.path.join(here, '../output/converted')
-        pnet.load_caffe_model(np.load(os.path.join(weight_folder, 'pnet.npy'))[()])
-        rnet.load_caffe_model(np.load(os.path.join(weight_folder, 'rnet.npy'))[()])
-        onet.load_caffe_model(np.load(os.path.join(weight_folder, 'onet.npy'))[()])
-        
-if __name__ == "__main__":
-    unittest.main()
