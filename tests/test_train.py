d0180476138e7eb78d50b67705827b1fd694a717
hanbing <beatmight@gmail.com>
2019-04-17 17:09:37 +0800

    change output dir

--- tests/test_train.py
+++ tests/test_train.py
@@ -4,7 +4,7 @@ from mtcnn.train.train_net import Trainer
 class TestTrain(unittest.TestCase):
 
     def test_train_pnet(self):
-        trainer = Trainer('pnet', device='cuda:0', log_dir='./runs/test/')
+        trainer = Trainer('pnet', device='cuda:0', log_dir='./runs/test/', output_folder='./runs/test/')
         trainer.train(20, 256, './output/test')
     
 
